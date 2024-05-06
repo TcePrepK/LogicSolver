57cd2800fa904ce2022dc000d43154a98cb7eaa7
TcePrepK <a.s.arda.simsek@gmail.com>
2024-05-06 03:27:27 +0300

    Ups, signal + WebStorm

---
+++ src/core/signal.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type EventHandler<T extends any[]> = (...args: T) => void;
+
+export class Signal<T extends any[]> {
+    private events: EventHandler<T>[] = [];
+
+    add(handler: (...args: T) => void): void {
+        this.events.push(handler);
+    }
+
+    dispatch(...args: T): void {
+        this.events.forEach(handler => handler(...args));
+    }
+}
